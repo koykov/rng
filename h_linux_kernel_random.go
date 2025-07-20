8066713fb02b3491adbe665270b266a734213af6
Konstantin Voykov <koykov0@gmail.com>
2025-07-21 00:26:04 +0300

    /dev/random: add ExpFloat64/NormFloat64 methods

--- h_linux_kernel_random.go
+++ h_linux_kernel_random.go
@@ -2,6 +2,7 @@ package rng
 
 import (
 	"encoding/binary"
+	"math"
 	"os"
 )
 
@@ -88,4 +89,55 @@ again:
 		goto again
 	}
 	return f
+}
+
+func (k KernelRandom) ExpFloat64() float64 {
+	for {
+		j := k.Uint32()
+		i := j & 0xFF
+		x := float64(j) * float64(we[i])
+		if j < ke[i] {
+			return x
+		}
+		if i == 0 {
+			return re - math.Log(k.Float64())
+		}
+		if fe[i]+float32(k.Float64())*(fe[i-1]-fe[i]) < float32(math.Exp(-x)) {
+			return x
+		}
+	}
+}
+
+func (k KernelRandom) NormFloat64() float64 {
+	for {
+		j := int32(k.Uint32())
+		i := j & 0x7F
+		x := float64(j) * float64(wn[i])
+		abs := uint32(j)
+		if j < 0 {
+			abs = uint32(-j)
+		}
+		if abs < kn[i] {
+			// This case should be hit better than 99% of the time.
+			return x
+		}
+
+		if i == 0 {
+			// This extra work is only required for the base strip.
+			for {
+				x = -math.Log(k.Float64()) * (1.0 / rn)
+				y := -math.Log(k.Float64())
+				if y+y >= x*x {
+					break
+				}
+			}
+			if j > 0 {
+				return rn + x
+			}
+			return -rn - x
+		}
+		if fn[i]+float32(k.Float64())*(fn[i-1]-fn[i]) < float32(math.Exp(-.5*x*x)) {
+			return x
+		}
+	}
 }
