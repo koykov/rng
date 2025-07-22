11e1e367bcb5e6dc5fa9bfa2aeb3dbd112343c5e
Konstantin Voykov <koykov0@gmail.com>
2025-07-22 23:39:16 +0300

    /dev/random: add Perm method

--- h_linux_kernel_random.go
+++ h_linux_kernel_random.go
@@ -141,3 +141,13 @@ func (k KernelRandom) NormFloat64() float64 {
 		}
 	}
 }
+
+func (k KernelRandom) Perm(n int) []int {
+	m := make([]int, n)
+	for i := 0; i < n; i++ {
+		j := k.Intn(i + 1)
+		m[i] = m[j]
+		m[j] = i
+	}
+	return m
+}
