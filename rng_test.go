8644a64d101eb64a4d7392cf1bea49d95ccf8464
Konstantin Voykov <koykov0@gmail.com>
2025-07-25 23:26:09 +0300

    /dev/random: cover with async tests

--- rng_test.go
+++ rng_test.go
@@ -1,12 +1,45 @@
 package rng
 
-import "testing"
+import (
+	"sync"
+	"testing"
+)
 
 func TestRNG(t *testing.T) {
-	t.Run("kernel/random", func(t *testing.T) {
-		if KernelRandom.Int63() == 0 {
-			t.Error()
+	testfn := func(t *testing.T, rng Interface, n int, async bool) {
+		if async {
+			var wg sync.WaitGroup
+			for i := 0; i < n; i++ {
+				wg.Add(1)
+				go func() {
+					defer wg.Done()
+					if rng.Int63() == 0 {
+						t.Error()
+					}
+				}()
+			}
+			wg.Wait()
+			return
 		}
+		for i := 0; i < n; i++ {
+			if rng.Int63() == 0 {
+				t.Error()
+			}
+		}
+	}
+	t.Run("kernel/random", func(t *testing.T) {
+		t.Run("sync", func(t *testing.T) {
+			t.Run("1", func(t *testing.T) { testfn(t, &KernelRandom, 1, false) })
+			t.Run("10", func(t *testing.T) { testfn(t, &KernelRandom, 1, false) })
+			t.Run("100", func(t *testing.T) { testfn(t, &KernelRandom, 1, false) })
+			t.Run("1000", func(t *testing.T) { testfn(t, &KernelRandom, 1, false) })
+		})
+		t.Run("async", func(t *testing.T) {
+			t.Run("1", func(t *testing.T) { testfn(t, &KernelRandom, 1, true) })
+			t.Run("10", func(t *testing.T) { testfn(t, &KernelRandom, 1, true) })
+			t.Run("100", func(t *testing.T) { testfn(t, &KernelRandom, 1, true) })
+			t.Run("1000", func(t *testing.T) { testfn(t, &KernelRandom, 1, true) })
+		})
 	})
 }
 
