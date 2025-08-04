409e344ed495aa11ea2acd9c863148746d7752c9
Konstantin Voykov <koykov0@gmail.com>
2025-08-04 23:03:41 +0300

    tests: monobit: final coverage

--- monobit_test.go
+++ monobit_test.go
@@ -4,25 +4,25 @@ import (
 	"math"
 	"math/bits"
 	"testing"
-	"unsafe"
 )
 
 func TestMonobit(t *testing.T) {
 	testfn := func(rng Interface, n int) float64 {
 		var n0, n1 float64
 		for i := 0; i < n; i++ {
-			v := rng.Float64()
-			u := *(*uint64)(unsafe.Pointer(&v))
-			t := bits.OnesCount64(u)
-			n1 += float64(t)
-			n0 += float64(64 - t)
+			v := rng.Uint64()
+			o := bits.OnesCount64(v)
+			n1 += float64(o)
+			n0 += float64(64 - o)
 		}
 		return math.Min(n0, n1) / math.Max(n0, n1)
 	}
 	testgroup := func(t *testing.T, rng Interface, n int) {
 		t.Run("", func(t *testing.T) {
 			rate := testfn(rng, n)
-			t.Log(rate) // todo check
+			if 1.0-rate > 0.01 {
+				t.Fail()
+			}
 		})
 	}
 	t.Run("kernel/random", func(t *testing.T) {
