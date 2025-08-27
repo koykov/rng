b3ec879f4ca8c7794cd4a17a7af58684aba260b5
Konstantin Voykov <koykov0@gmail.com>
2025-08-27 23:53:55 +0300

    lcg: cleanup

--- lcg.go
+++ lcg.go
@@ -8,32 +8,8 @@ func (r *lcg) Seed(v int64) {
 	r.seed = v
 }
 
-func (r *lcg) Int() int {
-	return int(r.Uint64())
-}
-
-func (r *lcg) Intn(n int) int {
-	return int(r.Uint64() % uint64(n))
-}
-
-func (r *lcg) Int31() int32 {
-	return int32(r.Uint64() >> 32)
-}
-
-func (r *lcg) Int31n(n int32) int32 {
-	return int32(r.Uint64() % uint64(n))
-}
-
 func (r *lcg) Int63() int64 {
 	return int64(r.Uint64())
-}
-
-func (r *lcg) Int63n(n int64) int64 {
-	return int64(r.Uint64() % uint64(n))
-}
-
-func (r *lcg) Uint32() uint32 {
-	return uint32(r.Uint64())
 }
 
 func (r *lcg) Uint64() uint64 {
