e84ade04bddbc17ea3bead5a9c91dbf4e2429dcc
Konstantin Voykov <koykov0@gmail.com>
2025-08-28 00:18:11 +0300

    lcg: describe container

--- lcg.go
+++ lcg.go
@@ -16,3 +16,29 @@ func (r *lcg) Uint64() uint64 {
 	r.seed = (r.a*r.seed + r.c) % r.m
 	return uint64(r.seed)
 }
+
+type lcgContainer struct {
+	ZXSpectrum    wrapper
+	Ranqd1        wrapper
+	BorlandCpp    wrapper
+	BorlandDelphi wrapper
+	TurboPascal   wrapper
+	Glibc         wrapper
+	ANSI_C        wrapper
+	MSVCpp        wrapper
+	MSVBasic      wrapper
+	RtlUniform    wrapper
+	MinstdRand    wrapper
+	MinstdRand0   wrapper
+	MMIX          wrapper
+	Musl          wrapper
+	Java          wrapper
+	POSIX         wrapper
+	Random0       wrapper
+	Cc65          wrapper
+	RANDU         wrapper
+}
+
+var LCG = &lcgContainer{
+	// todo fill me
+}
