c4766492e6c9cd1431bf66b0cf6c349dd57917f2
Konstantin Voykov <koykov0@gmail.com>
2025-08-28 23:28:22 +0300

    lcg: cover with test

--- rng_test.go
+++ rng_test.go
@@ -49,6 +49,158 @@ func TestRNG(t *testing.T) {
 			testgroup(t, KernelUrandom.Concurrent, true, 1, 10, 100, 1000)
 		})
 	})
+	t.Run("lcg/ZXSpectrum", func(t *testing.T) {
+		t.Run("sync", func(t *testing.T) {
+			testgroup(t, LCG.ZXSpectrum, false, 1, 10, 100, 1000)
+		})
+		t.Run("async", func(t *testing.T) {
+			testgroup(t, LCG.ZXSpectrum.Concurrent, true, 1, 10, 100, 1000)
+		})
+	})
+	t.Run("lcg/ranqd1", func(t *testing.T) {
+		t.Run("sync", func(t *testing.T) {
+			testgroup(t, LCG.Ranqd1, false, 1, 10, 100, 1000)
+		})
+		t.Run("async", func(t *testing.T) {
+			testgroup(t, LCG.Ranqd1.Concurrent, true, 1, 10, 100, 1000)
+		})
+	})
+	t.Run("lcg/Borland C++", func(t *testing.T) {
+		t.Run("sync", func(t *testing.T) {
+			testgroup(t, LCG.BorlandCpp, false, 1, 10, 100, 1000)
+		})
+		t.Run("async", func(t *testing.T) {
+			testgroup(t, LCG.BorlandCpp.Concurrent, true, 1, 10, 100, 1000)
+		})
+	})
+	t.Run("lcg/Borland Delphi", func(t *testing.T) {
+		t.Run("sync", func(t *testing.T) {
+			testgroup(t, LCG.BorlandDelphi, false, 1, 10, 100, 1000)
+		})
+		t.Run("async", func(t *testing.T) {
+			testgroup(t, LCG.BorlandDelphi.Concurrent, true, 1, 10, 100, 1000)
+		})
+	})
+	t.Run("lcg/Turbo Pascal", func(t *testing.T) {
+		t.Run("sync", func(t *testing.T) {
+			testgroup(t, LCG.TurboPascal, false, 1, 10, 100, 1000)
+		})
+		t.Run("async", func(t *testing.T) {
+			testgroup(t, LCG.TurboPascal.Concurrent, true, 1, 10, 100, 1000)
+		})
+	})
+	t.Run("lcg/glibc", func(t *testing.T) {
+		t.Run("sync", func(t *testing.T) {
+			testgroup(t, LCG.Glibc, false, 1, 10, 100, 1000)
+		})
+		t.Run("async", func(t *testing.T) {
+			testgroup(t, LCG.Glibc.Concurrent, true, 1, 10, 100, 1000)
+		})
+	})
+	t.Run("lcg/ANSI C", func(t *testing.T) {
+		t.Run("sync", func(t *testing.T) {
+			testgroup(t, LCG.ANSI_C, false, 1, 10, 100, 1000)
+		})
+		t.Run("async", func(t *testing.T) {
+			testgroup(t, LCG.ANSI_C.Concurrent, true, 1, 10, 100, 1000)
+		})
+	})
+	t.Run("lcg/Microsoft Visual C++", func(t *testing.T) {
+		t.Run("sync", func(t *testing.T) {
+			testgroup(t, LCG.MSVCpp, false, 1, 10, 100, 1000)
+		})
+		t.Run("async", func(t *testing.T) {
+			testgroup(t, LCG.MSVCpp.Concurrent, true, 1, 10, 100, 1000)
+		})
+	})
+	t.Run("lcg/Microsoft Visual Basic", func(t *testing.T) {
+		t.Run("sync", func(t *testing.T) {
+			testgroup(t, LCG.MSVBasic, false, 1, 10, 100, 1000)
+		})
+		t.Run("async", func(t *testing.T) {
+			testgroup(t, LCG.MSVBasic.Concurrent, true, 1, 10, 100, 1000)
+		})
+	})
+	t.Run("lcg/RtlUniform", func(t *testing.T) {
+		t.Run("sync", func(t *testing.T) {
+			testgroup(t, LCG.RtlUniform, false, 1, 10, 100, 1000)
+		})
+		t.Run("async", func(t *testing.T) {
+			testgroup(t, LCG.RtlUniform.Concurrent, true, 1, 10, 100, 1000)
+		})
+	})
+	t.Run("lcg/minstd_rand", func(t *testing.T) {
+		t.Run("sync", func(t *testing.T) {
+			testgroup(t, LCG.MinstdRand, false, 1, 10, 100, 1000)
+		})
+		t.Run("async", func(t *testing.T) {
+			testgroup(t, LCG.MinstdRand.Concurrent, true, 1, 10, 100, 1000)
+		})
+	})
+	t.Run("lcg/minstd_rand0", func(t *testing.T) {
+		t.Run("sync", func(t *testing.T) {
+			testgroup(t, LCG.MinstdRand0, false, 1, 10, 100, 1000)
+		})
+		t.Run("async", func(t *testing.T) {
+			testgroup(t, LCG.MinstdRand0.Concurrent, true, 1, 10, 100, 1000)
+		})
+	})
+	t.Run("lcg/MMIX", func(t *testing.T) {
+		t.Run("sync", func(t *testing.T) {
+			testgroup(t, LCG.MMIX, false, 1, 10, 100, 1000)
+		})
+		t.Run("async", func(t *testing.T) {
+			testgroup(t, LCG.MMIX.Concurrent, true, 1, 10, 100, 1000)
+		})
+	})
+	t.Run("lcg/Musl", func(t *testing.T) {
+		t.Run("sync", func(t *testing.T) {
+			testgroup(t, LCG.Musl, false, 1, 10, 100, 1000)
+		})
+		t.Run("async", func(t *testing.T) {
+			testgroup(t, LCG.Musl.Concurrent, true, 1, 10, 100, 1000)
+		})
+	})
+	t.Run("lcg/Java", func(t *testing.T) {
+		t.Run("sync", func(t *testing.T) {
+			testgroup(t, LCG.Java, false, 1, 10, 100, 1000)
+		})
+		t.Run("async", func(t *testing.T) {
+			testgroup(t, LCG.Java.Concurrent, true, 1, 10, 100, 1000)
+		})
+	})
+	t.Run("lcg/POSIX", func(t *testing.T) {
+		t.Run("sync", func(t *testing.T) {
+			testgroup(t, LCG.POSIX, false, 1, 10, 100, 1000)
+		})
+		t.Run("async", func(t *testing.T) {
+			testgroup(t, LCG.POSIX.Concurrent, true, 1, 10, 100, 1000)
+		})
+	})
+	t.Run("lcg/random0", func(t *testing.T) {
+		t.Run("sync", func(t *testing.T) {
+			testgroup(t, LCG.Random0, false, 1, 10, 100, 1000)
+		})
+		t.Run("async", func(t *testing.T) {
+			testgroup(t, LCG.Random0.Concurrent, true, 1, 10, 100, 1000)
+		})
+	})
+	t.Run("lcg/cc65", func(t *testing.T) {
+		t.Run("sync", func(t *testing.T) {
+			testgroup(t, LCG.Cc65, false, 1, 10, 100, 1000)
+		})
+		t.Run("async", func(t *testing.T) {
+			testgroup(t, LCG.Cc65.Concurrent, true, 1, 10, 100, 1000)
+		})
+	})
+	t.Run("lcg/RANDU", func(t *testing.T) {
+		t.Run("sync", func(t *testing.T) {
+			testgroup(t, LCG.RANDU, false, 1, 10, 100, 1000)
+		})
+		t.Run("async", func(t *testing.T) {
+			testgroup(t, LCG.RANDU.Concurrent, true, 1, 10, 100, 1000)
+		})
+	})
 }
 
 func BenchmarkRNG(b *testing.B) {
