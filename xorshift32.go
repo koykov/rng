806fdcaa20de1e5b1ac46c9d9d1be1a79b072a41
Konstantin Voykov <koykov0@gmail.com>
2025-09-05 23:25:31 +0300

    xorshift: 32: implementation

--- xorshift32.go
+++ xorshift32.go
@@ -1,14 +1,20 @@
 package rng
 
-type xorshift32 struct{}
+type xorshift32 struct {
+	a uint32
+}
 
-func (r *xorshift32) Seed(seed int64) {}
+func (r *xorshift32) Seed(_ int64) {}
 
 func (r *xorshift32) Int63() int64 {
 	return int64(r.Uint64())
 }
 
 func (r *xorshift32) Uint64() uint64 {
-	// todo implement me
-	return 0
+	x := r.a
+	x ^= x << 13
+	x ^= x >> 17
+	x ^= x << 5
+	r.a = x
+	return uint64(x)
 }
