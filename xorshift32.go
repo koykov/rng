8e225e73800791650026f6280378250eaa9b42d2
Konstantin Voykov <koykov0@gmail.com>
2025-09-06 22:30:02 +0300

    xorshift: 32: constructor

--- xorshift32.go
+++ xorshift32.go
@@ -4,7 +4,14 @@ type xorshift32 struct {
 	a uint32
 }
 
-func (r *xorshift32) Seed(_ int64) {}
+func newXorshift32(seed int64) *xorshift32 {
+	r := &xorshift32{a: uint32(seed)}
+	return r
+}
+
+func (r *xorshift32) Seed(seed int64) {
+	r.a = uint32(seed)
+}
 
 func (r *xorshift32) Int63() int64 {
 	return int64(r.Uint64())
