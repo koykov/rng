927add4cc6b7e5bf0cb1c2dff9d5cf322707288e
Konstantin Voykov <koykov0@gmail.com>
2025-09-08 22:21:30 +0300

    xoshiro: 256ss: implementation

---
+++ xoshiro256pp.go
@@ -0,0 +1,33 @@
+package rng
+
+type xoshiro256pp struct {
+	xoshiroBase
+}
+
+func newXoshiro256pp(seed uint64) *xoshiro256pp {
+	r := &xoshiro256pp{xoshiroBase{s: [4]uint64{seed}}}
+	return r
+}
+
+func (r *xoshiro256pp) Seed(seed int64) {
+	r.s[0] = uint64(seed)
+}
+
+func (r *xoshiro256pp) Int63() int64 {
+	return int64(r.Uint64())
+}
+
+func (r *xoshiro256pp) Uint64() uint64 {
+	result := r.rol64(r.s[0]+r.s[3], 23) + r.s[0]
+	t := r.s[1] << 17
+
+	r.s[2] ^= r.s[0]
+	r.s[3] ^= r.s[1]
+	r.s[1] ^= r.s[2]
+	r.s[0] ^= r.s[3]
+
+	r.s[2] ^= t
+	r.s[3] = r.rol64(r.s[3], 45)
+
+	return result
+}
