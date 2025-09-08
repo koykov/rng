01346fa7bb607d55cc32c2118e9501ad7e6ec6b6
Konstantin Voykov <koykov0@gmail.com>
2025-09-08 22:04:03 +0300

    xoshiro: 256p: implementation

---
+++ xoshiro256p.go
@@ -0,0 +1,33 @@
+package rng
+
+type xoshiro256p struct {
+	xoshiroBase
+}
+
+func newXoshiro256p(seed uint64) *xoshiro256p {
+	r := &xoshiro256p{xoshiroBase{s: [4]uint64{seed}}}
+	return r
+}
+
+func (r *xoshiro256p) Seed(seed int64) {
+	r.s[0] = uint64(seed)
+}
+
+func (r *xoshiro256p) Int63() int64 {
+	return int64(r.Uint64())
+}
+
+func (r *xoshiro256p) Uint64() uint64 {
+	result := r.s[0] + r.s[3]
+	t := r.s[1] << 17
+
+	r.s[2] ^= r.s[0]
+	r.s[3] ^= r.s[1]
+	r.s[1] ^= r.s[2]
+	r.s[0] ^= r.s[3]
+
+	r.s[2] ^= t
+	r.s[3] = r.rol64(r.s[3], 45)
+
+	return result
+}
